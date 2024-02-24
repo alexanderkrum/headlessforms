8033fa0dda6127f2bd56e5f001398701e2aaa3b3
Alexander Krum <alexander.krum.it@gmail.com>
2024-02-24 23:05:09 +1100

    fix: fix import

--- src/app/fontFamily.ts
+++ src/app/fontFamily.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import {Inter as FontSans} from "next/dist/compiled/@next/font/dist/google";
+import { Inter as FontSans } from "next/font/google"
 
 export const fontFamily = FontSans({
     subsets: ["latin"],
