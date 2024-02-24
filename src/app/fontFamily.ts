f8fa625f779be586f7ea19d02569d24c4ce375c6
Alexander Krum <alexander.krum.it@gmail.com>
2024-02-24 22:52:51 +1100

    chore: init project

---
+++ src/app/fontFamily.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import {Inter as FontSans} from "next/dist/compiled/@next/font/dist/google";
+
+export const fontFamily = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
